f41722a7d07dfffd03ceeb230e763ff11b0267c2
peng <robertjustinianpeng@gmail.com>
2025-08-24 14:18:03 +1000

    A: first commit

---
+++ app/core/config.py
@@ -0,0 +1,26 @@
+from re import S
+from pydantic_settings import BaseSettings
+from typing import Optional
+
+class Settings(BaseSettings):
+    
+    POSTGRES_SERVER: str
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str 
+    POSTGRES_DB: str
+    DATABASE_URL: str
+    
+    DEBUG: bool = False
+    PROJECT_NAME: str = "Data API Collector"
+    REDIS_URL: str
+    KAFKA_BOOTSTRAP_SERVERS: str
+    API_V1_STR: str
+    SECRET_KEY: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: str
+    
+    
+    class Config:
+        env_file = ".env"
+        case_sensitive = True
+
+settings = Settings()
